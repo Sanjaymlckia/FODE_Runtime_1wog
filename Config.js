3171fcbd80bedc653ee7e28e86e60e1080d868ae
Sanjay <sanjay@minervacenters.com>
2026-05-14 14:45:15 +1000

    release: r161 admin communications diagnostics

--- Config.js
+++ Config.js
@@ -7,9 +7,9 @@
  */
 var CONFIG = {
   // Versioning (change control)
-  VERSION: "r155",
+  VERSION: "r161",
 CHANGELOG_LAST: "r113: enforce mail identity alias guard and bounce mailbox alignment",
-  DEPLOY_VERSION_NUMBER: 155,
+  DEPLOY_VERSION_NUMBER: 161,
 BUILD_LABEL: "r113 enforce mail identity alias guard and bounce mailbox alignment",
 
   // STAGING/PROD data mode routing (working sheet)
