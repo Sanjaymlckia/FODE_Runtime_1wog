07e5f3d6a5eaba7d62309ca9498429fe692168e3
Sanjay <sanjay@minervacenters.com>
2026-06-12 20:06:58 +1000

    release: r224A legacy admin dashboard label polish

--- Config.js
+++ Config.js
@@ -7,9 +7,9 @@
  */
 var CONFIG = {
   // Versioning (change control)
-  VERSION: "r223B",
+  VERSION: "r224A",
 CHANGELOG_LAST: "r113: enforce mail identity alias guard and bounce mailbox alignment",
-  DEPLOY_VERSION_NUMBER: 223,
+  DEPLOY_VERSION_NUMBER: 224,
 BUILD_LABEL: "r113 enforce mail identity alias guard and bounce mailbox alignment",
 
   // STAGING/PROD data mode routing (working sheet)
