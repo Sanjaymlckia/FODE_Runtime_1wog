515eecc8125316410a413d0622cc5e7a906b647a
Sanjay <sanjay@minervacenters.com>
2026-06-12 19:13:43 +1000

    release: r222B legacy admin dashboard metric clarity patch

--- Config.js
+++ Config.js
@@ -7,9 +7,9 @@
  */
 var CONFIG = {
   // Versioning (change control)
-  VERSION: "r221B",
+  VERSION: "r222B",
 CHANGELOG_LAST: "r113: enforce mail identity alias guard and bounce mailbox alignment",
-  DEPLOY_VERSION_NUMBER: 221,
+  DEPLOY_VERSION_NUMBER: 222,
 BUILD_LABEL: "r113 enforce mail identity alias guard and bounce mailbox alignment",
 
   // STAGING/PROD data mode routing (working sheet)
