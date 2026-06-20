6577c80805d3845336a5ead3b40d15fb67db0d1a
Sanjay <sanjay@minervacenters.com>
2026-06-20 10:20:09 +1000

    E1D.1 metric and label reconciliation

--- Config.js
+++ Config.js
@@ -7,9 +7,9 @@
  */
 var CONFIG = {
   // Versioning (change control)
-  VERSION: "r276",
+  VERSION: "r277",
 CHANGELOG_LAST: "r113: enforce mail identity alias guard and bounce mailbox alignment",
-  DEPLOY_VERSION_NUMBER: 276,
+  DEPLOY_VERSION_NUMBER: 277,
 BUILD_LABEL: "r113 enforce mail identity alias guard and bounce mailbox alignment",
 
   // STAGING/PROD data mode routing (working sheet)
