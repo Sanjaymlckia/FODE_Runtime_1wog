f539d3f15749d1b9fe5526948c993b3baba4a88c
Sanjay <sanjay@minervacenters.com>
2026-06-22 08:37:05 +1000

    Restore stable AdminUI for r282 staging diagnostic

--- Config.js
+++ Config.js
@@ -7,9 +7,9 @@
  */
 var CONFIG = {
   // Versioning (change control)
-  VERSION: "r281",
+  VERSION: "r282",
 CHANGELOG_LAST: "r113: enforce mail identity alias guard and bounce mailbox alignment",
-  DEPLOY_VERSION_NUMBER: 281,
+  DEPLOY_VERSION_NUMBER: 282,
 BUILD_LABEL: "r113 enforce mail identity alias guard and bounce mailbox alignment",
 
   // STAGING/PROD data mode routing (working sheet)
