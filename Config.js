b6cb197aaeb6702e5a74236665e2c3146b6c115e
Sanjay <sanjay@minervacenters.com>
2026-06-21 19:28:07 +1000

    Bump identity for D1Y.5 staging diagnostic

--- Config.js
+++ Config.js
@@ -7,9 +7,9 @@
  */
 var CONFIG = {
   // Versioning (change control)
-  VERSION: "r278",
+  VERSION: "r281",
 CHANGELOG_LAST: "r113: enforce mail identity alias guard and bounce mailbox alignment",
-  DEPLOY_VERSION_NUMBER: 278,
+  DEPLOY_VERSION_NUMBER: 281,
 BUILD_LABEL: "r113 enforce mail identity alias guard and bounce mailbox alignment",
 
   // STAGING/PROD data mode routing (working sheet)
