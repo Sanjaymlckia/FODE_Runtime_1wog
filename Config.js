517cfde091f91884ca102b4797c66456e4cbaa5d
Sanjay <sanjay@minervacenters.com>
2026-06-22 16:07:12 +1000

    Bump identity for H3 selected communication staging

--- Config.js
+++ Config.js
@@ -7,9 +7,9 @@
  */
 var CONFIG = {
   // Versioning (change control)
-  VERSION: "r284",
+  VERSION: "r285",
 CHANGELOG_LAST: "r113: enforce mail identity alias guard and bounce mailbox alignment",
-  DEPLOY_VERSION_NUMBER: 284,
+  DEPLOY_VERSION_NUMBER: 285,
 BUILD_LABEL: "r113 enforce mail identity alias guard and bounce mailbox alignment",
 
   // STAGING/PROD data mode routing (working sheet)
