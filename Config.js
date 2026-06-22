06203824435b53b14cda5cbfbbcffe611bfd1892
Sanjay <sanjay@minervacenters.com>
2026-06-22 11:54:30 +1000

    Bump identity for H2 template guidance staging

--- Config.js
+++ Config.js
@@ -7,9 +7,9 @@
  */
 var CONFIG = {
   // Versioning (change control)
-  VERSION: "r283",
+  VERSION: "r284",
 CHANGELOG_LAST: "r113: enforce mail identity alias guard and bounce mailbox alignment",
-  DEPLOY_VERSION_NUMBER: 283,
+  DEPLOY_VERSION_NUMBER: 284,
 BUILD_LABEL: "r113 enforce mail identity alias guard and bounce mailbox alignment",
 
   // STAGING/PROD data mode routing (working sheet)
