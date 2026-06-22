2e194e7b3cc7992394772ea49526a24f41bbb067
Sanjay <sanjay@minervacenters.com>
2026-06-22 11:05:44 +1000

    Bump identity for H1 semantic registry staging

--- Config.js
+++ Config.js
@@ -7,9 +7,9 @@
  */
 var CONFIG = {
   // Versioning (change control)
-  VERSION: "r282",
+  VERSION: "r283",
 CHANGELOG_LAST: "r113: enforce mail identity alias guard and bounce mailbox alignment",
-  DEPLOY_VERSION_NUMBER: 282,
+  DEPLOY_VERSION_NUMBER: 283,
 BUILD_LABEL: "r113 enforce mail identity alias guard and bounce mailbox alignment",
 
   // STAGING/PROD data mode routing (working sheet)
