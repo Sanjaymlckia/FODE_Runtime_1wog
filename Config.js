a347dcda2ac5e869285c5d30ee40534a54f647d7
Sanjay <sanjay@minervacenters.com>
2026-06-29 11:33:53 +1000

    chore: bump identity for H6E communications staging

--- Config.js
+++ Config.js
@@ -7,9 +7,9 @@
  */
 var CONFIG = {
   // Versioning (change control)
-  VERSION: "r301",
+  VERSION: "r302",
 CHANGELOG_LAST: "r113: enforce mail identity alias guard and bounce mailbox alignment",
-  DEPLOY_VERSION_NUMBER: 301,
+  DEPLOY_VERSION_NUMBER: 302,
 BUILD_LABEL: "r113 enforce mail identity alias guard and bounce mailbox alignment",
 
   // STAGING/PROD data mode routing (working sheet)
