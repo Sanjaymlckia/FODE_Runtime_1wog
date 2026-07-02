5fb7c34f631d806fb65885ea5be730ff0913be0b
Sanjay <sanjay@minervacenters.com>
2026-07-02 23:41:26 +1000

    chore: bump identity for communication authority enforcement

--- Config.js
+++ Config.js
@@ -7,9 +7,9 @@
  */
 var CONFIG = {
   // Versioning (change control)
-  VERSION: "r309",
+  VERSION: "r310",
 CHANGELOG_LAST: "r113: enforce mail identity alias guard and bounce mailbox alignment",
-  DEPLOY_VERSION_NUMBER: 309,
+  DEPLOY_VERSION_NUMBER: 310,
 BUILD_LABEL: "r113 enforce mail identity alias guard and bounce mailbox alignment",
 
   // STAGING/PROD data mode routing (working sheet)
