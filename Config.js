f4aecd6bae6100f168a7444c7849d85bc4dbf0d4
Sanjay <sanjay@minervacenters.com>
2026-07-08 17:53:32 +1000

    chore: bump runtime identity for canonical lifecycle diagnostics

--- Config.js
+++ Config.js
@@ -7,9 +7,9 @@
  */
 var CONFIG = {
   // Versioning (change control)
-  VERSION: "r334",
+  VERSION: "r335",
 CHANGELOG_LAST: "r113: enforce mail identity alias guard and bounce mailbox alignment",
-  DEPLOY_VERSION_NUMBER: 334,
+  DEPLOY_VERSION_NUMBER: 335,
 BUILD_LABEL: "r113 enforce mail identity alias guard and bounce mailbox alignment",
 
   // STAGING/PROD data mode routing (working sheet)
