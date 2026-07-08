c19d4e19e15a0031b59ad4b1bd7a00e283e0264b
Sanjay <sanjay@minervacenters.com>
2026-07-08 12:56:16 +1000

    chore: bump runtime identity for communication authority messaging

--- Config.js
+++ Config.js
@@ -7,9 +7,9 @@
  */
 var CONFIG = {
   // Versioning (change control)
-  VERSION: "r332",
+  VERSION: "r333",
 CHANGELOG_LAST: "r113: enforce mail identity alias guard and bounce mailbox alignment",
-  DEPLOY_VERSION_NUMBER: 332,
+  DEPLOY_VERSION_NUMBER: 333,
 BUILD_LABEL: "r113 enforce mail identity alias guard and bounce mailbox alignment",
 
   // STAGING/PROD data mode routing (working sheet)
