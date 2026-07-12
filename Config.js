9fc4b2bb5612a4d7cf946793c65ca66bb80c330e
Sanjay <sanjay@minervacenters.com>
2026-07-12 15:05:09 +1000

    chore: record Track H release identity

--- Config.js
+++ Config.js
@@ -7,9 +7,9 @@
  */
 var CONFIG = {
   // Versioning (change control)
-  VERSION: "r338",
+  VERSION: "r339",
 CHANGELOG_LAST: "r113: enforce mail identity alias guard and bounce mailbox alignment",
-  DEPLOY_VERSION_NUMBER: 338,
+  DEPLOY_VERSION_NUMBER: 339,
 BUILD_LABEL: "r113 enforce mail identity alias guard and bounce mailbox alignment",
 
   // STAGING/PROD data mode routing (working sheet)
