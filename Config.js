592f46732a3dbc7e6bd4253acb5f9ff88793d0b5
Sanjay <sanjay@minervacenters.com>
2026-07-13 20:39:12 +1000

    chore: bump runtime identity for finance convergence release

--- Config.js
+++ Config.js
@@ -7,9 +7,9 @@
  */
 var CONFIG = {
   // Versioning (change control)
-  VERSION: "r342",
+  VERSION: "r343",
 CHANGELOG_LAST: "r113: enforce mail identity alias guard and bounce mailbox alignment",
-  DEPLOY_VERSION_NUMBER: 342,
+  DEPLOY_VERSION_NUMBER: 343,
 BUILD_LABEL: "r113 enforce mail identity alias guard and bounce mailbox alignment",
 
   // STAGING/PROD data mode routing (working sheet)
