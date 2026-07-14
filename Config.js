86da669f717ee6d430a5c5773f4c7f7d733f80e7
Sanjay <sanjay@minervacenters.com>
2026-07-14 13:26:37 +1000

    chore: bump runtime identity for current admin readability acceptance

--- Config.js
+++ Config.js
@@ -7,9 +7,9 @@
  */
 var CONFIG = {
   // Versioning (change control)
-  VERSION: "r346",
+  VERSION: "r347",
 CHANGELOG_LAST: "r113: enforce mail identity alias guard and bounce mailbox alignment",
-  DEPLOY_VERSION_NUMBER: 346,
+  DEPLOY_VERSION_NUMBER: 347,
 BUILD_LABEL: "r113 enforce mail identity alias guard and bounce mailbox alignment",
 
   // STAGING/PROD data mode routing (working sheet)
