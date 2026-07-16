c0e80888aaffeff84253a8945f075922c16cc5ec
Sanjay <sanjay@minervacenters.com>
2026-07-16 18:44:57 +1000

    chore: bump runtime identity for EduOps Pass 2 staging

--- Config.js
+++ Config.js
@@ -7,9 +7,9 @@
  */
 var CONFIG = {
   // Versioning (change control)
-  VERSION: "r352",
+  VERSION: "r353",
 CHANGELOG_LAST: "r113: enforce mail identity alias guard and bounce mailbox alignment",
-  DEPLOY_VERSION_NUMBER: 352,
+  DEPLOY_VERSION_NUMBER: 353,
 BUILD_LABEL: "r113 enforce mail identity alias guard and bounce mailbox alignment",
 
   // STAGING/PROD data mode routing (working sheet)
