207f0203778eedc5b55f5976de6b8d3f5a2f63fc
Sanjay <sanjay@minervacenters.com>
2026-07-19 00:14:20 +1000

    chore: bump runtime identity to r362

--- Config.js
+++ Config.js
@@ -7,9 +7,9 @@
  */
 var CONFIG = {
   // Versioning (change control)
-  VERSION: "r361",
+  VERSION: "r362",
 CHANGELOG_LAST: "r113: enforce mail identity alias guard and bounce mailbox alignment",
-  DEPLOY_VERSION_NUMBER: 361,
+  DEPLOY_VERSION_NUMBER: 362,
 BUILD_LABEL: "r113 enforce mail identity alias guard and bounce mailbox alignment",
 
   // STAGING/PROD data mode routing (working sheet)
