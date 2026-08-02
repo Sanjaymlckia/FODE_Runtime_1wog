6da310d426e45c6937a5f9199dc7c9143ddad048
Sanjay <sanjay@kundu.ac>
2026-08-03 00:08:06 +1000

    chore: set R406 release identity

--- Config.js
+++ Config.js
@@ -7,9 +7,9 @@
  */
 var CONFIG = {
   // Versioning (change control)
-  VERSION: "r403",
+  VERSION: "r406",
 CHANGELOG_LAST: "r113: enforce mail identity alias guard and bounce mailbox alignment",
-  DEPLOY_VERSION_NUMBER: 403,
+  DEPLOY_VERSION_NUMBER: 406,
 BUILD_LABEL: "r113 enforce mail identity alias guard and bounce mailbox alignment",
 
   // STAGING/PROD data mode routing (working sheet)
